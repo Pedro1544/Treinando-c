0f8496414d4b70dc0fbba4afd728d060e338d659
Pedro1544 <mith3221@gmail.com>
2023-08-31 00:31:45 -0300

    primeiro commit

---
+++ bin/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Declaracoes de variaveis
+    int idade = 26;
+    float valorDoPgto = 12.42;
+    double velParticular = 2.9258756;
+    char tipoHabMotor = 'A';
+
+    // instrucoes
+    printf("idade: %d\n", idade);
+    printf("Valor do pagamento: %f \n", valorDoPgto);
+    printf("Velocidade da particula: %lf \n", velParticular);
+    printf("Tipo de habilitação: %c", tipoHabMotor);
+    return 0;
+}
